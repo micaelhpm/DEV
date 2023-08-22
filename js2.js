5de2383427c77a159c76dacc5366c8f55d12894b
Micael Henrique Pereira Morais <pmicaelhenrique@gmail.com>
2023-08-22 09:34:43 -0300

    AULA 2208

---
+++ js2.js
@@ -0,0 +1,27 @@
+// soma de três números
+
+let num1 = 2;
+
+let num2= 4;
+
+let num3 = 6;
+
+let soma1= num1 + num2 + num3;
+
+console.log(soma1); 
+
+let decimal1 = 2.5;
+
+let decimal2 = 1.3;
+
+let soma2 = decimal1 + decimal2;
+
+console.log(soma2); 
+
+let texto1 = "Olá";
+
+let texto2 = "mundo";
+
+let mensagem = texto1 + " " + texto2;
+
+console.log(mensagem);
---
+++ js3.js
@@ -0,0 +1,31 @@
+let subtracao1 = 8-3;
+
+console.log(subtracao1); 
+
+let decimal1 = 5.7;
+
+let decimal2 = 2.3;
+
+let subtracao2 = decimal1 - decimal2;
+
+console.log(subtracao2); 
+
+let num1 = 10;
+
+let num2 = 3;
+
+let resultado = num1 - num2;
+
+console.log(resultado); 
+
+let divisao1 = 10/2;
+
+console.log(divisao1); 
+
+let decimal11 = 5.5;
+
+let decimal22 = 2;
+
+let divisao2 = decimal11 / decimal22;
+
+console.log(divisao2); 

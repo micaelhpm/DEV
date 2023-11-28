96d13590677a7986e198b0c90b4eb19cc8188114
Micael Henrique Pereira Morais <pmicaelhenrique@gmail.com>
2023-11-28 11:12:27 -0300

    TCCFINALIZADO

---
+++ TCCFINALIZADO/loginpage.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="style.css">
+<div class="center">
+      <form action="">
+        <div class="title">Registre sua Opnião</div>
+        <span class="inputs">
+            <span class="inputf">
+                <input type="nome" class="input" placeholder="Nome" />
+                <span class="label">Nome</span>             
+              </span>
+            <span class="inputf">
+                <input type="email" class="input" placeholder="email"/>
+                <span class="label">email</span>
+              </span>
+          <span class="inputf">
+            <input type="opniao" class="input" placeholder="Sua opnião" />
+            <span class="label">Sua opnião</span>
+            <span class="material-icons icon">email</span>
+          </span>
+        </span>
+        <button type="button" class="btn">
+          <span>ENVIAR</span>         
+        </button>   8
